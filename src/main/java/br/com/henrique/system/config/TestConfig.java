7585d7c751303014e2fd3c950baac80cbdff79d9
henrique brendon <henriquebrendonti26@gmail.com>
2024-03-26 14:40:18 -0300

    controllers of the product entity

--- src/main/java/br/com/henrique/system/config/TestConfig.java
+++ src/main/java/br/com/henrique/system/config/TestConfig.java
@@ -2,6 +2,8 @@ package br.com.henrique.system.config;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -20,7 +22,11 @@ public class TestConfig implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        Product product = new Product(null, "test", new BigDecimal("300.00"), new BigDecimal("300.00"), Instant.now(), Instant.now(), 2);
-        repository.save(product);
+        List<Product> list = new ArrayList<>();
+        for(int i = 0; i<10; i++){
+            list.add(new Product(null, "test", new BigDecimal("300.00"), new BigDecimal("300.00"), Instant.now(), Instant.now(), 2));
+        }
+        
+        repository.saveAll(list);
     }
 }
--- src/main/java/br/com/henrique/system/controllers/ProductController.java
+++ src/main/java/br/com/henrique/system/controllers/ProductController.java
@@ -1,17 +1,63 @@
 package br.com.henrique.system.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.henrique.system.entities.Product;
+import br.com.henrique.system.services.ProductService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
     
-    /*
-    @GetMapping
-    public ResponseEntity<Product> findALl(){
-        
+    private ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service =  service;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable long id) {
+        Product product = service.findById(id);
         return ResponseEntity.ok().body(product);
     }
-     */
+
+    @PostMapping()
+    public ResponseEntity<Product> insert(@RequestBody Product entity) {
+        entity = service.insert(entity);
+        return ResponseEntity.ok().body(entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product entity) {
+        entity = service.update(id, entity);
+        return ResponseEntity.ok().body(entity);
+    }
+    
+    
 }
