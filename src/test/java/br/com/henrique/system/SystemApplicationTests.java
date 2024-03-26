ab427b6d079eafe28053f07533b58d3f27d36cda
henrique brendon <henriquebrendonti26@gmail.com>
2024-03-26 13:36:19 -0300

    Project structure created

---
+++ src/test/java/br/com/henrique/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.henrique.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
